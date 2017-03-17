6474bb4ba0a38c5bc3918249f1ed29db97b18736
vanderleiot <central@hostbdc.com.br>
2017-03-16 22:42:30 -0300

    app
    hostbdc

--- www/js/config.js
+++ www/js/config.js
@@ -1,29 +1,29 @@
-angular.module('WordApp.config', [])
 
-// Replace with your Wordpress Blog WP REST API url 
-.constant('WORDPRESS_API_URL', '<WORDPRESS_API_URL>')
 
-// Replace with your Google Project Number https://documentation.onesignal.com/v2.0/docs/android-generating-a-gcm-push-notification-key
-.constant('GOOGLE_PROJECT_NUMBER', '<GOOGLE_PROJECT_NUMBER>')
+// Substituir por seu Wordpress Blog WP REST API url 
+.constant ('WORDPRESS_API_URL', '<http://hostbdc.com.br/wp-json/wp/v2/')
 
-// Replace with your OneSignal AppId https://documentation.onesignal.com/docs/phonegap-sdk-installation#2-add-required-code
-.constant('ONESIGNAL_APP_ID', '<ONESIGNAL_APP_ID>')
+// Substitua por seu Número do Projeto do Google https://documentation.onesignal.com/v2.0/docs/android-generating-a-gcm-push-notification-key
+.constant ('GOOGLE_PROJECT_NUMBER', '<GOOGLE_PROJECT_NUMBER>')
 
-// Change color your Ionic application to light, stable, positive, calm, balanced, energized, assertive, royal or dark 
-.constant('IONIC_APP_COLOR', 'positive')
+// Substitua com o OneSignal AppId https://documentation.onesignal.com/docs/phonegap-sdk-installation#2-add-required-code
+.constant ('ONESIGNAL_APP_ID', '<ONESIGNAL_APP_ID>')
 
-// Change Posts page template to cards, cards2, list
-.constant('POSTS_TEMPLATE', 'cards')
+// Muda a cor da sua aplicação iónica para luz, estável, positiva, calma, equilibrada, energizada, assertiva, real ou escura 
+.constant ('IONIC_APP_COLOR', 'positivo')
 
-// Replace with your Android package name and IOS app ID
-.constant('ANDROID_PACKAGE_NAME', 'com.google.android.apps.maps')
-.constant('IOS_APP_ID', '<my_app_id>')
+// Alterar modelo de página de posts para cartões, cards2, list
+.constant ('POSTS_TEMPLATE', 'cards')
 
-// Replace with your Primary Email to send Feedback
-.constant('PRIMARY_EMAIL', 'app@myapp.com')
+// Substitua pelo nome do pacote do Android e pelo ID do aplicativo do IOS
+.constant ('ANDROID_PACKAGE_NAME', 'com.google.android.apps.maps')
+.constant ('IOS_APP_ID', '<my_app_id>')
 
-// Replace with email to send copy Feedback or leave empty
-.constant('COPY_EMAIL', 'app2@myapp.com')
+// Substitua com seu email preliminar para emitir o feedback
+.constant ('PRIMARY_EMAIL', 'app@myapp.com')
 
-// Replace with phone number for send SMS
-.constant('SMS_PHONE_NUMBER', '<SMS_PHONE_NUMBER>');
+// Substituir por e-mail para enviar cópia Comentários ou deixar vazio
+.constant ('COPY_EMAIL', 'app2@myapp.com')
+
+// Substituir por número de telefone para enviar SMS
+.constant ('SMS_PHONE_NUMBER', '<SMS_PHONE_NUMBER>');
